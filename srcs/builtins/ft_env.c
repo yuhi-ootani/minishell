03e82b883eb7bff91c6037c91fb82c938bad7747
Otani Yuhi <oyuhi@c4r2s17.42tokyo.jp>
2025-03-13 16:05:32 +0900

    env and unset

--- srcs/builtins/ft_env.c
+++ srcs/builtins/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: oyuhi <oyuhi@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 15:42:21 by knemcova          #+#    #+#             */
-/*   Updated: 2025/03/12 20:05:37 by oyuhi            ###   ########.fr       */
+/*   Updated: 2025/03/13 12:49:15 by oyuhi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,8 @@ int	ft_env(t_command *command, t_env *coppied_env)
 	tmp = coppied_env;
 	while (tmp)
 	{
-		printf("%s=%s\n", tmp->name, tmp->value);
+		if (tmp->value)
+			printf("%s=%s\n", tmp->name, tmp->value);
 		tmp = tmp->next;
 	}
 	return (0);
--- srcs/builtins/ft_unset.c
+++ srcs/builtins/ft_unset.c
@@ -6,24 +6,60 @@
 /*   By: oyuhi <oyuhi@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 15:44:32 by knemcova          #+#    #+#             */
-/*   Updated: 2025/03/12 20:12:48 by oyuhi            ###   ########.fr       */
+/*   Updated: 2025/03/13 15:04:06 by oyuhi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+// int	ft_unset(t_command *command, t_env *copied_env)
+// {
+// 	(void)copied_env;
+// 	if (!command->args[1])
+// 	{
+// 		fprintf(stderr, "unset: argument not found\n");
+// 		return (1);
+// 	}
+// 	if (unsetenv(command->args[1]) != 0)
+// 	{
+// 		perror("unset");
+// 		return (1);
+// 	}
+// 	return (0);
+// }
+
+static void	remove_target_env(t_env **copied_env, t_env *prev, t_env *tmp)
+{
+	if (prev)
+		prev->next = tmp->next;
+	else
+		*copied_env = tmp->next;
+	free(tmp->name);
+	free(tmp->value);
+	free(tmp);
+}
+
 int	ft_unset(t_command *command, t_env *copied_env)
 {
-	(void)copied_env;
-	if (!command->args[1])
+	size_t	i;
+	char	*var_name;
+	t_env	*prev;
+	t_env	*tmp;
+
+	i = 1;
+	while (command->args[i])
 	{
-		fprintf(stderr, "unset: argument not found\n");
-		return (1);
+		var_name = command->args[i];
+		prev = NULL;
+		tmp = copied_env;
+		while (tmp)
+		{
+			if (ft_strcmp(tmp->name, var_name) == 0)
+				remove_target_env(&copied_env, prev, tmp);
+			prev = tmp;
+			tmp = tmp->next;
+		}
+		i++;
 	}
-	if (unsetenv(command->args[1]) != 0)
-	{
-		perror("unset");
-		return (1);
-	}
-	return (0);
+	return (0); // to do
 }
