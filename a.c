712fcc3f3ec634f70098f0c6863086094f6f02eb
Yuhi Otani <maron0602inbabb@gmail.com>
2025-03-31 21:23:37 +0900

    merge

--- a.c
+++ a.c
@@ -3,14 +3,11 @@
 
 int	main(void)
 {
-	printf(" World!\n");
-	printf(" World!\n");
-	printf(" World!\n");
-	printf(" World!\n");
-	printf(" World!\n");
-	printf("Hello, World!\n");
-	printf("Hello, World!\n");
-	printf("Hello, World!\n");
-	printf("Hello, World!\n");
+	char *parent = (char *)malloc(10);
+	int pid = fork();
+	if (pid == 0)
+	{
+		char *child = (char *)malloc(10);
+	}
 	return (1);
 }
